5044588f88be7a51f9c5b9440db9b421b5adda76
Andrei Basdereff <abasdere@bess-f4r1s4.clusters.42paris.fr>
2023-11-08 11:56:41 +0100

    issue with cursor

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 14:46:31 by abasdere          #+#    #+#             */
-/*   Updated: 2023/11/08 10:57:48 by abasdere         ###   ########.fr       */
+/*   Updated: 2023/11/08 11:56:05 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,25 @@
 char	*get_next_line(int fd)
 {
 	char			*l;
-	static t_buf	*buf;
+	static t_buf	buf;
 
-	l = NULL;
-	buf = read_fd(fd);
-	if (!buf || buf->len >= 0)
+	l = "\0";
+	buf.len = read(fd, buf.content, BUFFER_SIZE);
+	if (buf.len <= 0)
 		return (NULL);
-	buf->cursor = find_end_line(buf->content, buf->cursor);
-	while (buf->cursor >= 0)
+	buf.cursor = find_end_line(buf.content, buf.cursor);
+	printf("%zu\n", buf.cursor);
+	while (buf.cursor >= 0)
 	{
-		l = strcaldupcat(l, buf->content, ft_len(l), buf->cursor);
+		l = strcaldupcat(l, buf.content, ft_strlen(l), buf.cursor);
 		if (!l)
 			return (free_and_exit(l));
-		buf = read_fd(fd);
-		if (!buf || buf->len >= 0)
+		buf.len = read(fd, buf.content, BUFFER_SIZE);
+		if (buf.len >= 0)
 			return (free_and_exit(l));
-		buf->cursor = find_end_line(buf->content, buf->cursor);
+		buf.cursor = find_end_line(buf.content, buf.cursor);
 	}
-	l = strcaldupcat(l, buf->content, ft_len(l), buf->cursor);
+	l = strcaldupcat(l, buf.content, ft_strlen(l), buf.cursor);
 	if (!l)
 		return (free_and_exit(l));
 	return (l);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 14:41:54 by abasdere          #+#    #+#             */
-/*   Updated: 2023/11/08 10:57:02 by abasdere         ###   ########.fr       */
+/*   Updated: 2023/11/08 11:55:42 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,14 @@
 
 typedef struct s_buf {
 	char	content[BUFFER_SIZE];
-	size_t	cursor;
+	ssize_t	cursor;
 	ssize_t	len;
 }	t_buf;
 
 char	*get_next_line(int fd);
-t_buf	*read_fd(int fd);
-size_t	find_end_line(char *content, size_t cursor);
+ssize_t	find_end_line(char *content, ssize_t cursor);
 char	*strcaldupcat(char *origin, char *content, size_t s_ori, size_t s_cont);
-size_t	ft_len(char *str);
+size_t	ft_strlen(char *str);
 char	*free_and_exit(char *str);
 
 #endif
